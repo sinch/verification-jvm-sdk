058be7ba76ea10381e372ccb87ce1b0d4282dc1d
Aleksander Wójcik <aleksander.wojcik@sinch.com>
2020-11-04 13:27:24 +0100

    Added simple sample showing SDK usage

--- sample/src/main/kotlin/com/sinch/verificationsample/Main.kt
+++ sample/src/main/kotlin/com/sinch/verificationsample/Main.kt
@@ -1,12 +1,60 @@
 package com.sinch.verificationsample
 
+import com.sinch.verification.Verification
+import com.sinch.verification.model.VerificationMethodType
+import com.sinch.verification.model.initiation.InitiationResponseData
+import com.sinch.verification.network.auth.AppKeyAuthorizationMethod
+import com.sinch.verification.process.config.VerificationMethodConfig
+import com.sinch.verification.process.listener.InitiationListener
+import com.sinch.verification.process.listener.VerificationListener
+import com.sinch.verification.process.method.VerificationMethod
+
 class Main {
 
-    companion object {
+    companion object : VerificationListener, InitiationListener {
+
+        private const val APP_KEY = "9e556452-e462-4006-aab0-8165ca04de66"
+        private var verification: Verification? = null
+
         @JvmStatic
         fun main(args: Array<String>) {
-            println("Kotlin main is running here!")
+            val phoneNumber = inputWithMessage("Enter number that needs to be verified")
+            val verificationConfig = VerificationMethodConfig.Builder.instance
+                .authorizationMethod(AppKeyAuthorizationMethod(appKey = APP_KEY))
+                .verificationMethod(VerificationMethodType.SMS)
+                .number(phoneNumber)
+                .build()
+
+            verification = VerificationMethod.Builder.instance
+                .verificationConfig(verificationConfig)
+                .verificationListener(this)
+                .initiationListener(this)
+                .build()
+
+            verification?.initiate()
         }
+
+        override fun onVerified() {
+            println("Successfully verified phone number!")
+        }
+
+        override fun onVerificationFailed(t: Throwable) {
+            println("Error while verifying the code: ${t.localizedMessage}")
+        }
+
+        override fun onInitiated(data: InitiationResponseData) {
+            verification?.verify(inputWithMessage("Enter received code"))
+        }
+
+        override fun onInitializationFailed(t: Throwable) {
+            println("Error while initiation verification: ${t.localizedMessage}")
+        }
+
+        private fun inputWithMessage(msg: String): String {
+            println(msg)
+            return readLine().orEmpty()
+        }
+
     }
 
 }
